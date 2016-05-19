73351d0103a525d91ff8cecf2345bea6b24c4422
Po-yuan Paul Kuo <GMP2K@Po-yuans-MacBook-Pro.local>
2016-05-19 12:12:56 -0700

    Initial Commit

---
+++ SPA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SPA
+//
+//  Created by Po-yuan Paul Kuo on 5/19/16.
+//  Copyright (c) 2016 Po-yuan Paul Kuo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
